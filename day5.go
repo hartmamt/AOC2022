5cb1adf247c7f707dac4d3e7571e6c6f044f3d20
Matthew Hartman <hartman.m@pg.com>
2022-12-05 19:35:14 -0500

    part 2

--- day5.go
+++ day5.go
@@ -8,6 +8,96 @@ import (
 	"strings"
 )
 
+func main() {
+	// Define the path of the file to read.
+	filePath := "day5input.txt"
+
+	// Open the file for reading.
+	file, err := os.Open(filePath)
+	if err != nil {
+		// If an error occurred, print it and return.
+		fmt.Println(err)
+		return
+	}
+	defer file.Close()
+
+	// Make nine stacks
+	stacks := make([]Stack, 9)
+	stacksPartTwo := make([]Stack, 9)
+
+	// Create a new scanner for the file.
+	scanner := bufio.NewScanner(file)
+	counter := 0
+
+	for scanner.Scan() {
+
+		line := scanner.Text()
+
+		if counter < 8 {
+			// Scan lines into crates
+			for i := 0; i < 35; i++ {
+				crate := line[i : i+3]
+				column := i / 4
+				// Push the crate onto the stack
+				if crate != "   " {
+					stacks[column].Load(crate)
+					stacksPartTwo[column].Load(crate)
+				}
+				i = i + 3
+			}
+		} else if counter > 9 {
+			// Now parse the moves
+			parts := strings.Split(line, " ")
+			if parts[0] == "move" {
+				// How many creates to move
+				num, _ := strconv.Atoi(parts[1])
+				// Which stack to move from
+				from, _ := strconv.Atoi(parts[3])
+				// Which stack to move to
+				to, _ := strconv.Atoi(parts[5])
+
+				// This loop is for part one
+				for i := 0; i < num; i++ {
+					// move them crates one at a time (part one)
+					if stacks[from-1].Len() > 0 {
+						stacks[to-1].Push(stacks[from-1].Pop())
+					}
+				}
+
+				// This loop is for part two
+				// We need a temporary stack to hold the crates
+				tempStack := Stack{}
+				// Load them stacks into a stack
+				for i := 0; i < num; i++ {
+					if stacksPartTwo[from-1].Len() > 0 {
+						tempStack.Push(stacksPartTwo[from-1].Pop())
+					}
+				}
+				// Dump that stack onto the other stack
+				for i := 0; i < num; i++ {
+					stacksPartTwo[to-1].Push(tempStack.Pop())
+				}
+
+			}
+		}
+		counter++
+	}
+
+	// Print the answers
+
+	fmt.Println()
+	fmt.Println("Top of Stacks Part 1:")
+	for i := 0; i < 9; i++ {
+		fmt.Print(stacks[i].Peek())
+	}
+
+	fmt.Println()
+	fmt.Println("Top of Stacks Part 2:")
+	for i := 0; i < 9; i++ {
+		fmt.Print(stacksPartTwo[i].Peek())
+	}
+}
+
 // Stack is a simple stack data structure
 type Stack struct {
 	items []string
@@ -44,86 +134,4 @@ func (s *Stack) Peek() string {
 // Len returns the number of items in the stack
 func (s *Stack) Len() int {
 	return len(s.items)
-}
-
-func main() {
-	// Define the path of the file to read.
-	filePath := "day5input.txt"
-
-	// Open the file for reading.
-	file, err := os.Open(filePath)
-	if err != nil {
-		// If an error occurred, print it and return.
-		fmt.Println(err)
-		return
-	}
-	defer file.Close()
-
-	// Make nine stacks
-	stacks := make([]Stack, 9)
-
-	// Create a new scanner for the file.
-	scanner := bufio.NewScanner(file)
-	counter := 0
-
-	for scanner.Scan() {
-
-		line := scanner.Text()
-
-		if counter < 8 {
-			// Scan lines into crates
-			for i := 0; i < 35; i++ {
-				crate := line[i : i+3]
-				column := i / 4
-				// Push the crate onto the stack
-				if crate != "   " {
-					stacks[column].Load(crate)
-				}
-				i = i + 3
-			}
-		} else if counter > 9 {
-			// Split the string by spaces
-			parts := strings.Split(line, " ")
-			// Parse the move command
-			if parts[0] == "move" {
-				num, _ := strconv.Atoi(parts[1])
-				from, _ := strconv.Atoi(parts[3])
-				to, _ := strconv.Atoi(parts[5])
-
-				fmt.Printf("Moving %d crates from stack %d to stack %d\n", num, from, to)
-
-				for i := 0; i < num; i++ {
-					// Pop the crates from the stack
-					if stacks[from-1].Len() > 0 {
-						stacks[to-1].Push(stacks[from-1].Pop())
-					}
-				}
-			}
-		}
-		if counter == 8 {
-			// Print the stacks
-			fmt.Println()
-			for i := 0; i < 9; i++ {
-				fmt.Printf("Stack %d: ", i+1)
-				for j := 0; j < stacks[i].Len(); j++ {
-					fmt.Printf("%s ", stacks[i].items[j])
-				}
-				fmt.Println()
-			}
-			fmt.Println()
-		}
-		counter++
-	}
-
-	fmt.Println()
-	fmt.Println("Ending Stacks:")
-	for i := 0; i < 9; i++ {
-		fmt.Println(stacks[i])
-	}
-	fmt.Println()
-	fmt.Println("Top of Stacks:")
-	for i := 0; i < 9; i++ {
-		fmt.Print(stacks[i].Peek())
-	}
-	//[M][Q][T][P][G][L][L][D][N]
 }
